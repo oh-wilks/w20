1e8e21aaab64f25f1c0ba4db84ed958427442c49
oh-wilks <wilcoxivan@hotmail.com>
2023-07-01 08:56:56 -0700

    intial config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 // primer paso, leer variables de entorno
 require('dotenv').config
+
+const express = require("express");
+const app = express();
+
+//middle ware
+
+app.use(express.json());
+
+//ruta de bienvenida
+app.get("/api", (req, res) => {
+    return res.json({
+        msg: "Welcome to API"
+    })
+})
+
+// iniciar applicacion 
+app.listen(process.env.PORT, () => {
+    console.log(`Servivor en puerto ${process.env.PORT}`);
+});
